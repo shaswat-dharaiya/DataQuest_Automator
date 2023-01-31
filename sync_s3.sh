e5dc7b560510ea17e0870b22396647e96c9749c2
Shaswat Dharaiya <shaswat.dharaiya@gmail.com>
2023-01-31 13:11:46 -0500

    update action

--- sync_s3.sh
+++ sync_s3.sh
@@ -2,4 +2,21 @@
 
 set -e
 
-echo "Hello" ${AWS_S3_BUCKET} "Bucket"
+aws configure --profile rearc-quest-aws <<-EOF > /dev/null 2>&1
+${AWS_ACCESS_KEY_ID}
+${AWS_SECRET_ACCESS_KEY}
+${AWS_REGION}
+text
+EOF
+
+sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
+              --profile rearc-quest-aws \
+              --no-progress \
+              ${ENDPOINT_APPEND} $*"
+
+aws configure --profile s3-sync-action <<-EOF > /dev/null 2>&1
+null
+null
+null
+text
+EOF
