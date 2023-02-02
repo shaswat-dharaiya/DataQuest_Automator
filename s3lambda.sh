cee97695d0f82a7fcc4bc86626a2b89b824ac98c
Shaswat Dharaiya <shaswat.dharaiya@gmail.com>
2023-02-02 03:16:36 -0500

    update action

--- s3lambda.sh
+++ s3lambda.sh
@@ -1,6 +1,8 @@
 # Exit immediately
 set -e
 
+
+
 AWS_REGION="us-east-1"
 aws configure --profile rearc-quest-aws <<-EOF > /dev/null 2>&1
 ${AWS_ACCESS_KEY_ID}
@@ -8,6 +10,11 @@ ${AWS_SECRET_ACCESS_KEY}
 ${AWS_REGION}
 text
 EOF
+
+echo ${AWS_S3_BUCKET_LAMBDA}
+echo ${AWS_ACCESS_KEY_ID}
+echo ${AWS_SECRET_ACCESS_KEY}
+echo ${AWS_REGION}
 
 # Use the profile to connect to the s3 bucket
 sh -c "aws s3 sync /home/runner/work/Rearc-Quest/Rearc-Quest/env/lib/*/*/lambda_function.zip s3://${AWS_S3_BUCKET_LAMBDA}/ \
