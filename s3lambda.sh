6082d06ae1ddc758af41306b160e7705a1522629
Shaswat Dharaiya <shaswat.dharaiya@gmail.com>
2023-02-02 02:21:34 -0500

    update action

--- s3lambda.sh
+++ s3lambda.sh
@@ -2,6 +2,8 @@ pip install virtualenv
 cd $HOME
 virtualenv -p /usr/bin/* Lambda
 source Lambda/bin/activate
+pwd
+ls ./
 pip install beautifulsoup4
 pip install lxml
 cp /home/runner/work/Rearc-Quest/Rearc-Quest/lambda/s3_script.py ~/Lambda/lib/*/*/
@@ -11,32 +13,25 @@ zip -r9 lambda_function.zip
 #!/bin/sh
 
 # Exit immediately
-set -e
+# set -e
 
-AWS_REGION="us-east-1"
+# AWS_REGION="us-east-1"
+# aws configure --profile rearc-quest-aws <<-EOF > /dev/null 2>&1
+# ${AWS_ACCESS_KEY_ID}
+# ${AWS_SECRET_ACCESS_KEY}
+# ${AWS_REGION}
+# text
+# EOF
 
-# configure aa profile and save the credentials to that profile.
-# >> /dev/null redirects standard output (stdout) to /dev/null, which discards it.
-# 2>&1 redirects standard error (2) to standard output (1),
-# which then discards it as well since standard output has already been redirected.
-# & indicates a file descriptor.
-# There are usually 3 file descriptors - standard input, output, and error.
-aws configure --profile rearc-quest-aws <<-EOF > /dev/null 2>&1
-${AWS_ACCESS_KEY_ID}
-${AWS_SECRET_ACCESS_KEY}
-${AWS_REGION}
-text
-EOF
+# # Use the profile to connect to the s3 bucket
+# sh -c "aws s3 cp ./lambda_function.zip s3://${AWS_S3_BUCKET_LAMBDA}/ \
+#               --profile rearc-quest-aws \
+#               --no-progress $*"
 
-# Use the profile to connect to the s3 bucket
-sh -c "aws s3 cp ./lambda_function.zip s3://${AWS_S3_BUCKET_LAMBDA}/ \
-              --profile rearc-quest-aws \
-              --no-progress $*"
-
-# Unset the variables.
-aws configure --profile rearc-quest-aws <<-EOF > /dev/null 2>&1
-null
-null
-null
-text
-EOF
+# # Unset the variables.
+# aws configure --profile rearc-quest-aws <<-EOF > /dev/null 2>&1
+# null
+# null
+# null
+# text
+# EOF
