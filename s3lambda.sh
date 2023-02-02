4245e1a850fe392f83cc3cd3f397b23b24cb6da6
Shaswat Dharaiya <shaswat.dharaiya@gmail.com>
2023-02-02 02:36:51 -0500

    update action

--- s3lambda.sh
+++ s3lambda.sh
@@ -12,7 +12,7 @@ set -e
 # cp /home/runner/work/Rearc-Quest/Rearc-Quest/lambda/s3_script.py ~/Lambda/lib/*/*/
 # cd ~/Lambda/lib/*/*/
 # zip -r9 lambda_function.zip
-echo ${VIRTUAL_ENV}
+echo "${VIRTUAL_ENV}"
 
 # AWS_REGION="us-east-1"
 # aws configure --profile rearc-quest-aws <<-EOF > /dev/null 2>&1
