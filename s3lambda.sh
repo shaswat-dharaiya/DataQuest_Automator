ac50cd45e1a61d7714888e10308608af4e17f616
Shaswat Dharaiya <shaswat.dharaiya@gmail.com>
2023-02-02 02:50:11 -0500

    update action

--- s3lambda.sh
+++ s3lambda.sh
@@ -1,36 +1,25 @@
 # Exit immediately
 set -e
 
-# pip install virtualenv
-# cd $HOME
-# virtualenv -p /usr/bin/* Lambda
-# source Lambda/bin/activate
-# pwd
-# ls ./
-# pip install beautifulsoup4
-# pip install lxml
-# cp /home/runner/work/Rearc-Quest/Rearc-Quest/lambda/s3_script.py ~/Lambda/lib/*/*/
-# cd ~/Lambda/lib/*/*/
-# zip -r9 lambda_function.zip
-echo "VE: ${VIRTUAL_ENV} ${VIRTUAL_ENV1}"
+VE="/home/runner/work/Rearc-Quest/Rearc-Quest/env"
 
-# AWS_REGION="us-east-1"
-# aws configure --profile rearc-quest-aws <<-EOF > /dev/null 2>&1
-# ${AWS_ACCESS_KEY_ID}
-# ${AWS_SECRET_ACCESS_KEY}
-# ${AWS_REGION}
-# text
-# EOF
+AWS_REGION="us-east-1"
+aws configure --profile rearc-quest-aws <<-EOF > /dev/null 2>&1
+${AWS_ACCESS_KEY_ID}
+${AWS_SECRET_ACCESS_KEY}
+${AWS_REGION}
+text
+EOF
 
-# # Use the profile to connect to the s3 bucket
-# sh -c "aws s3 cp ./lambda_function.zip s3://${AWS_S3_BUCKET_LAMBDA}/ \
-#               --profile rearc-quest-aws \
-#               --no-progress $*"
+# Use the profile to connect to the s3 bucket
+sh -c "aws s3 cp /home/runner/work/Rearc-Quest/Rearc-Quest/env/lib/*/*/lambda_function.zip s3://${AWS_S3_BUCKET_LAMBDA}/ \
+              --profile rearc-quest-aws \
+              --no-progress $*"
 
-# # Unset the variables.
-# aws configure --profile rearc-quest-aws <<-EOF > /dev/null 2>&1
-# null
-# null
-# null
-# text
-# EOF
+# Unset the variables.
+aws configure --profile rearc-quest-aws <<-EOF > /dev/null 2>&1
+null
+null
+null
+text
+EOF
