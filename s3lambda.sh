4b51c5f3b3db2a8c55e2594e0f6cc325ceb915bb
Shaswat Dharaiya <shaswat.dharaiya@gmail.com>
2023-02-02 01:31:12 -0500

    update action

--- s3lambda.sh
+++ s3lambda.sh
@@ -1,7 +1,11 @@
-pip install virtualenv
-cd $HOME
-virtualenv -p /usr/bin/python3.6 Lambda
-Lambda BSoup/bin/activate
-pip install beautifulsoup4
-pip install lxml
-cp 
+# pip install virtualenv
+# cd $HOME
+# virtualenv -p /usr/bin/python3 Lambda
+# Lambda BSoup/bin/activate
+# pip install beautifulsoup4
+# pip install lxml
+# cp 
+
+python -v
+ls /usr/bin/
+tree ./
