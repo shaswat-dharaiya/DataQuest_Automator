4fda03809128e65a5c73faa410c556c48cda9f85
Shaswat Dharaiya <shaswat.dharaiya@gmail.com>
2023-02-03 03:49:05 -0500

    update tf

--- scripts/TF_Init.sh
+++ scripts/TF_Init.sh
@@ -1,11 +1,13 @@
 set -e
 cd ./user/
+terraform destroy -var "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}" -var "AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}" --auto-approve 
 terraform init
 terraform plan -var "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}" -var "AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}"
 terraform apply -var "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}" -var "AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}" --auto-approve 
 ls
 
 cd ../buckets/
+terraform destroy --auto-approve 
 terraform init
 terraform plan
 terraform apply --auto-approve
